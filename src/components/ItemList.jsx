dc0a11db815dc4412821e57ecb816d7621179569
Ashab-Asir <ashabasir101@gmail.com>
2024-06-24 11:42:56 +0600

    item list,input and buttton added

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+
+const initialItems = [
+  {
+    name: "Good mood",
+    packed: true,
+  },
+  {
+    name: "Passport",
+    packed: false,
+  },
+  {
+    name: "Phone Charger",
+    packed: false,
+  },
+];
 export default function ItemList() {
-  return <div>item list</div>;
+  const [items, setItems] = useState(initialItems);
+  return (
+    <ul>
+      {items.map((item) => {
+        return <Item key={item.name} item={item}></Item>;
+      })}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" checked={item.packed}></input>
+        {item.name}
+      </label>
+      <button>❌</button>
+    </li>
+  );
 }
