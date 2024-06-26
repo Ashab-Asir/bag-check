1c22ff7ce4e14deff3c16fb2ad4c15b6fc948af5
Ashab-Asir <ashabasir101@gmail.com>
2024-06-26 07:42:01 +0600

    sorting item functionality added

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,49 @@
+import ReactSelect from "react-select";
 import EmptyView from "./EmptyView";
+import { useState } from "react";
 
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const sortingOptions = [
+    {
+      label: "sort by default",
+      value: "default",
+    },
+    {
+      label: "sort by packed",
+      value: "packed",
+    },
+    {
+      label: "sort by unpacked",
+      value: "unpacked",
+    },
+  ];
+  const [sortBy, setSortBy] = useState("default");
+  const sortedItem = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+    }
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return items;
+  });
   return (
     <ul className="item-list">
       {items.length === 0 ? <EmptyView></EmptyView> : null}
-      {items.map((item) => {
+      {items.length > 0 ? (
+        <section className="sorting">
+          <ReactSelect
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+            onChange={(option) => setSortBy(option.value)}
+          ></ReactSelect>
+        </section>
+      ) : null}
+      {sortedItem.map((item) => {
         return (
           <Item
             handleDeleteItem={handleDeleteItem}
